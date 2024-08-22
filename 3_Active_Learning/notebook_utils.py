5ef5feb08ee40284d78ba1056439240932a9cf6f
Youssef Ouakaa <youssef.ouakaa2@gmail.com>
2024-08-22 04:02:27 -0400

    Add active learning example

---
+++ 3_Active_Learning/notebook_utils.py
@@ -0,0 +1,147 @@
+from imblearn.over_sampling import SMOTE
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, roc_auc_score
+import seaborn as sns
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import average_precision_score
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+import os
+import re
+import gc
+import seaborn as sns
+
+
+def plot_confusion_matrix(model_name, Y_true, Y_pred, labels=["Benign", "Attack"]):
+    matrix = confusion_matrix(Y_true.is_attack, Y_pred)
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(matrix, annot=True, cmap='Blues', fmt='d', xticklabels=labels, yticklabels=labels)
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.title(f'Confusion Matrix for {model_name}')
+    plt.show()
+
+def metrics_report(dataset_type, y_true, y_predict, print_avg=True):
+    print(f"Classification Report ({dataset_type}):")
+    print(classification_report(y_true, y_predict, digits=4))
+    accuracy = accuracy_score(y_true, y_predict)
+    precision = precision_score(y_true, y_predict, average='weighted')
+    recall = recall_score(y_true, y_predict, average='weighted')
+    f1 = f1_score(y_true, y_predict, average='weighted')
+    auc = roc_auc_score(y_true, y_predict)
+    print("Accuracy:", accuracy)
+    print("Precision:", precision)
+    print("Recall:", recall)
+    print("F1 Score:", f1)
+    print("AUC:", auc)
+    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1, "auc": auc}
+
+def calculate_metrics_by_label(y_true, y_pred, labels):
+    results = []
+    unique_labels = labels.unique()
+    for label in unique_labels:
+        indices = labels == label
+        accuracy = accuracy_score(y_true[indices], y_pred[indices])
+        results.append({
+            'Label': label,
+            'Accuracy': accuracy,
+        })
+    return pd.DataFrame(results)
+
+def test_metrics(model_name, model, dataset_type, scaler, X_test, Y_test):
+    # Predict and evaluate on the test set
+    print(f"{model_name} with {dataset_type} Test Set Performance")
+    Y_pred = model.predict(scaler.transform(X_test))
+    metrics = metrics_report(f"Test {model_name} ({dataset_type})", Y_test.is_attack, Y_pred)
+    plot_confusion_matrix(f"{model_name} ({dataset_type})", Y_test, Y_pred)
+    # Calculate metrics by label
+    metrics_by_label = calculate_metrics_by_label(Y_test.is_attack, Y_pred, Y_test.label)
+    metrics_by_label['Method'] = dataset_type
+    print(f"Metrics by Label ({dataset_type}):")
+    print(metrics_by_label)
+    return metrics, metrics_by_label
+
+# Define the updated test_metrics_DL function with reshape parameter
+def test_metrics_DL(model_name, model, scaler, X_test, Y_test, reshape=False):
+    # Reshape data before normalizing
+    if reshape:
+        X_test_reshaped = X_test.reshape(-1, X_test.shape[1], 1)
+        X_test_normalized = scaler.transform(X_test_reshaped.reshape(-1, X_test.shape[1])).reshape(-1, X_test.shape[1], 1)
+    else:
+        X_test_normalized = scaler.transform(X_test)
+    Y_pred = (model.predict(X_test_normalized) > 0.5).astype("int32")
+
+    metrics = metrics_report(f"Test {model_name} ({"SMOTE"})", Y_test.is_attack, Y_pred)
+    plot_confusion_matrix(f"{model_name} ({"SMOTE"})", Y_test, Y_pred)
+    # Calculate metrics by label
+    metrics_by_label = calculate_metrics_by_label(Y_test.is_attack, Y_pred, Y_test.label)
+    metrics_by_label['Method'] = "SMOTE"
+    print(f"Metrics by Label ({"SMOTE"}):")
+    print(metrics_by_label)
+    return metrics, metrics_by_label
+
+# Define the updated test_metrics_DL function with reshape parameter
+def test_metrics_AE(model_name, model, scaler, X_test, Y_test, threshold = 95):
+    X_test_normalized = scaler.transform(X_test)
+    # Perform reconstruction using the Autoencoder
+    reconstructions = model.predict(X_test_normalized)
+    # Calculate Mean Squared Error (MSE)
+    mse = np.mean(np.power(X_test_normalized - reconstructions, 2), axis=1)
+    # Set a threshold for anomaly detection (95th percentile of the training MSE)
+    threshold = np.percentile(mse, threshold)
+    Y_pred = (mse > threshold).astype(int)
+    metrics = metrics_report(f"Test {model_name} ({"SMOTE"})", Y_test.is_attack, Y_pred)
+    plot_confusion_matrix(f"{model_name} ({"SMOTE"})", Y_test, Y_pred)
+    # Calculate metrics by label
+    metrics_by_label = calculate_metrics_by_label(Y_test.is_attack, Y_pred, Y_test.label)
+    metrics_by_label['Method'] = "SMOTE"
+    print(f"Metrics by Label ({"SMOTE"}):")
+    print(metrics_by_label)
+    return metrics, metrics_by_label
+
+def test_metrics_AE_new(model_name, model, scaler, X_test, Y_test, threshold):
+    X_test_normalized = scaler.transform(X_test)
+    # Perform reconstruction using the Autoencoder
+    reconstructions = model.predict(X_test_normalized)
+    # Calculate Mean Absolute Error (MAE)
+    test_loss = np.mean(np.abs(X_test_normalized - reconstructions), axis=1)
+    # Classify as anomaly if reconstruction error > threshold
+    Y_pred = (test_loss > threshold).astype(int)
+    metrics = metrics_report(f"Test {model_name} ({"SMOTE"})", Y_test.is_attack, Y_pred)
+    plot_confusion_matrix(f"{model_name} ({"SMOTE"})", Y_test, Y_pred)
+    # Calculate metrics by label
+    metrics_by_label = calculate_metrics_by_label(Y_test.is_attack, Y_pred, Y_test.label)
+    metrics_by_label['Method'] = "SMOTE"
+    print(f"Metrics by Label ({"SMOTE"}):")
+    print(metrics_by_label)
+    return metrics, metrics_by_label
+    
+    return Y_pred, metrics_by_label
+    
+def plot_overall_accuracy(metrics):
+    methods = ['original', 'random', 'smote', 'adasyn']
+    overall_accuracies = []
+
+    # Extract overall accuracy for each method
+    for method in methods:
+        overall_accuracies.append(metrics[method][0]['accuracy'])
+
+    # Plotting the overall accuracies
+    plt.figure(figsize=(10, 6))
+    bars = plt.bar(methods, overall_accuracies, color=['blue', 'orange', 'green', 'red'])
+    plt.title('Overall Accuracy by Method')
+    plt.xlabel('Method')
+    plt.ylabel('Accuracy')
+    plt.ylim(0.9, 1)
+    plt.grid(True)
+
+    # Display the values on each bar
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2.0, yval, f'{yval:.5f}', ha='center', va='bottom')
+
+    plt.show()
